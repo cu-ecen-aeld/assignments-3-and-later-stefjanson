337e432a76b4ddda1832b92222f31e372d8b04e9
Stef Janson <sjanson@lely.com>
2023-12-15 15:11:50 +0100

    Write logging to syslog

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <syslog.h>
 
 int main(int argc, char *argv[]) {
+    // Open syslog with a specific identifier and options
+    openlog("writer_program", LOG_PID, LOG_USER);
+
     if (argc != 3) {
-        printf("./writer writefile writestr\n");
+        // Log an error message to syslog
+        syslog(LOG_ERR, "Usage: %s writefile writestr", argv[0]);
+        // Close syslog before exiting
+        closelog();
         return 1;
     }
 
@@ -12,12 +19,21 @@ int main(int argc, char *argv[]) {
 
     FILE *file = fopen(writefile, "w");
     if (file == NULL) {
-        printf("File could not be created\n");
+        // Log an error message to syslog
+        syslog(LOG_ERR, "File could not be created");
+        // Close syslog before exiting
+        closelog();
         return 1;
     }
 
     fprintf(file, "%s", writestr);
     fclose(file);
+
+    // Log a success message to syslog
+    syslog(LOG_INFO, "Write to file successful");
+
+    // Close syslog before exiting
+    closelog();
 
     return 0;
 }
