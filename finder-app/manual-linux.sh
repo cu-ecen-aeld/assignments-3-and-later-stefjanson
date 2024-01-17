b9a0251e32f77bd0643f71202e892159ae03a881
Stef Janson <sjanson@lely.com>
2024-01-17 17:16:34 +0100

    build linux

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -13,6 +13,8 @@ FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
 
+export PATH=$PATH:${FINDER_APP_DIR}/../../cross-compiler/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin
+
 if [ $# -lt 1 ]
 then
 	echo "Using default directory ${OUTDIR} for output"
@@ -23,24 +25,32 @@ fi
 
 mkdir -p ${OUTDIR}
 
-cd "$OUTDIR"
+
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+    cd "$OUTDIR"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+    cd -
 fi
+
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
-    cd linux-stable
+    cd "$OUTDIR/linux-stable"
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
-
     # TODO: Add your kernel build steps here
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+    cd -
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
+
 
 echo "Creating the staging directory for the root filesystem"
-cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
@@ -48,33 +58,62 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -p "$OUTDIR/rootfs"
+cd "$OUTDIR/rootfs"
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
+cd -
 
-cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
-    cd busybox
+    git clone git://busybox.net/busybox.git ${OUTDIR}
+    cd ${OUTDIR}/busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
-    cd busybox
+    cd ${OUTDIR}/busybox
 fi
 
 # TODO: Make and install busybox
+make -j8 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make -j8 CONFIG_PREFIX=../rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+${CROSS_COMPILE}readelf -a busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a busybox | grep "Shared library"
+
+cd -
 
 # TODO: Add library dependencies to rootfs
+# Interpreter
+cp ${FINDER_APP_DIR}/../needed_libs/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+# Libraries
+cp ${FINDER_APP_DIR}/../needed_libs/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp ${FINDER_APP_DIR}/../needed_libs/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp ${FINDER_APP_DIR}/../needed_libs/libc.so.6 ${OUTDIR}/rootfs/lib64
 
 # TODO: Make device nodes
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make clean
+make
+cd -
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp -r ${FINDER_APP_DIR}/* "${OUTDIR}/rootfs/home"
 
 # TODO: Chown the root directory
+sudo chown root:root "${OUTDIR}/rootfs"
 
 # TODO: Create initramfs.cpio.gz
+cd "${OUTDIR}/rootfs"
+find . | cpio -H newc -ov --owner root:root > ../initramfs.cpio
+gzip -f ../initramfs.cpio
+cd -
